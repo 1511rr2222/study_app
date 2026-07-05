543f84ab69c08d9e11301adfd43513d9adf9b200
1511rr2222 <1511rr2222@gmail.com>
2026-07-05 15:47:44 +0900

    mobile

---
+++ sw.js
@@ -0,0 +1,17 @@
+// 최소한의 서비스워커입니다.
+// 목적: PWA 설치(installable) 조건 중 하나인 "fetch 이벤트를 처리하는 서비스워커"를
+// 충족시키기 위함이며, 개발 중인 앱이라 공격적인 캐싱은 넣지 않았습니다.
+// (캐싱을 세게 걸면 코드를 수정해도 예린이 폰에 옛날 버전이 남아있는 문제가 생길 수 있어요.)
+
+self.addEventListener('install', (event) => {
+    self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(self.clients.claim());
+});
+
+// 네트워크를 그대로 통과시킴 (오프라인 캐싱 없음 - 최신 버전을 항상 받아오도록)
+self.addEventListener('fetch', (event) => {
+    event.respondWith(fetch(event.request));
+});
